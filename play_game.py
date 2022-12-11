40931d3f0577dee2e10f39763bf6861aeca4bb2c
Adam Drewing <adam195@gmail.com>
2022-12-11 15:06:03 +0100

    Create play_game.py

---
+++ play_game.py
@@ -0,0 +1,40 @@
+import random
+choices=['Rock','Paper','Scissors']
+name=input("Enter your name :")
+PlayerScore=0 
+Player2Score=0
+NumberOfRounds=0
+gameOn=True
+print(f"Welcome to game: {name.title()}")
+while NumberOfRounds<3:
+  PlayerOption=random.choice(choices)
+  Player2Option=input("Enter Rock/ Paper/ Scissors :").title()
+  print(f"Player option :{PlayerOption}")
+  print(f"{name.title()} option :{Player2Option}")
+  NumberOfRounds += 1
+  if PlayerOption == Player2Option:
+    print('Tie')
+  elif (PlayerOption =='Rock' and Player2Option == 'Scissors') or (PlayerOption =='Scissos' and Player2Option =='Paper') or (PlayerOption =='Paper' and Player2Option =='Rock'):
+    print("Player win")
+    PlayerScore += 1
+  elif (Player2Option =='Rock' and PlayerOption == 'Scissors') or (Player2Option =='Scissors' and PlayerOption =='Paper') or (Player2Option =='Paper' and PlayerOption =='Rock'):
+    print(f"{name.title()} Wins")
+    Player2Score += 1
+  else:
+   print("Choose option to play this game.") 
+  print("---")
+  print("")
+  print(f"Round No: {NumberOfRounds}")
+  print("--- Score Board ---")
+  print(f"{name.title()}: {Player2Score} | Player: {PlayerScore}")
+  print("===")
+  print("")
+  if NumberOfRounds==3:
+    gameOn=False
+    break
+if Player2Score==PlayerScore:
+  print("Draw!!")
+elif Player2Score>Player2Score:
+  print(f"Congrats {name.title()}, You won the game!!")
+else:
+  print(f"Player won the game!! Next time {name.title()}!")
